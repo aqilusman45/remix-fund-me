03524fe531de528c52b31ca0c561d29fba22bbd4
aqilusman45 <aqilusman45@gmail.com>
2022-07-24 10:45:16 -0500

    Optimizations - immutable - constant

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,9 +8,18 @@ pragma solidity ^0.8.0;
 
 import './PriceConverter.sol';
 
-contract FundMe {
-    address public owner;
+// optimizations
+// - immutable
+//   - Naming Convention: prefix with `i_`.
+// - constant
+//   - Name Convention: all caps snake casing  
 
+contract FundMe {
+    // adding immutable to the variable
+    // will compile the value for it in bytecode
+    // this means it won't take up space in the memory
+    // which reduces computation and saves gas. 
+    address public immutable i_owner;
     // constructor is a function that runs
     // immediately when the contract is
     // instantiated or is deployed in a transaction 
@@ -18,7 +27,7 @@ contract FundMe {
         // so for the fundme contract we want to save
         // who the deployer of the contract is
         // and the same person would be the owner as well
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     // syntax to import and use libraries
@@ -42,7 +51,7 @@ contract FundMe {
 
     // also lets maintain how much each sender funded
     mapping(address => uint256) public fundsRegister;
-    int256 public minimumUSD = 50 * 1e18;
+    int256 public constant MINIMUM_USD = 50 * 1e18;
 
     function fund() public payable {
         // How do we send eth to this contract?
@@ -72,7 +81,7 @@ contract FundMe {
             // the function is automatically invoked with the
             // value of the variable or field it is being called on as its argument
             // other arguments however would still need to be passed.
-            msg.value.getConversionRate() >= minimumUSD,
+            msg.value.getConversionRate() >= MINIMUM_USD,
             "Donation amount too low!"
         ); // all the transactions occur in smallest unit of eth i.e is Wei.
         // so 1e18 is equal to 1000000000000000000 = 1 eth.
@@ -120,7 +129,7 @@ contract FundMe {
     // modifying, in this case we are modifying withdraw function
     // with the condition in onlyOwner modifier.
     modifier onlyOwner() {
-        require(msg.sender == owner, "You don't own this contract.");
+        require(msg.sender == i_owner, "You don't own this contract.");
         _; // the underscore indicates that do what the rest of the function then follows.
     }
 }
